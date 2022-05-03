b356e15c123b5f42826e47ad02fd39d85dee31d9
Holy One <69742571+TTheHolyOne@users.noreply.github.com>
2022-05-03 18:55:54 +0000

    ye

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ const password = '010101';
 work = document.getElementById('work');
 main = document.getElementById('main');
 wrong = document.getElementById('wrong');
+hacking = document.getElementById('hacking');
+cracking = document.getElementById('cracking');
+types = document.getElementById('types');
 
+function hackingg() {
+	hacking.style.display = 'block';
+	types.style.display = 'none'
+}
+
+function crackingg() {
+	cracking.style.display = 'block';
+	types.style.display = 'none'
+}
+
+function back() {
+	cracking.style.display = 'none';
+	hacking.style.display = 'none';
+	types.style.display = 'block';
+	return false;
+}
 
 function getVal() {
-  const val = document.getElementById('text').value;
-  console.log(val);
-	if (val == password){
+	const val = document.getElementById('text').value;
+	console.log(val);
+	if (val == password) {
 		work.style.display = "block";
 		main.style.display = "none";
 	} else {
